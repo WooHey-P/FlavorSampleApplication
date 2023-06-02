d14f66f5cf48dc8c5236b6419bb5c4cfb4f782bf
박우현 <dkqnwlnd@naver.com>
2023-06-02 17:22:41 +0900

    flavor 적용

--- app/src/main/java/com/example/flavorsampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/flavorsampleapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.flavorsampleapplication
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView).text = packageName
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startActivity(Intent(this@MainActivity, SubActivity::class.java))
+        }
     }
 }
